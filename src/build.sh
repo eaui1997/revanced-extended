82eba2d8d65dcc00811fc88cf1ff5b64561581fa
manhduonghn <manhduonghn@live.com>
2023-06-09 03:44:24 +0700

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -34,6 +34,7 @@ patch "youtube" "yt-rve-v$version" "arm64-v8a"
 
 # Patch YouTube Music Extended 
 get_patches_key "ytmusic"
+version="6.04.53"
 #get_apkmirror "youtube-music" "arm64-v8a"
 get_uptodown "youtube-music"
 patch "youtube-music" "yt-music-v$version" "arm64-v8a"
