b0c8e897e37977fb88a96e9efb9f36c8a78b9aa0
manhduonghn <manhduonghn@live.com>
2023-06-08 07:04:17 +0700

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -30,13 +30,13 @@ get_patches_key "youtube"
 get_ver "youtube-rve"
 get_apkmirror "youtube"
 #get_uptodown "youtube"
-patch "youtube" "yt-rve-v$version"
+patch "youtube" "yt-rve-v$version" "arm64-v8a"
 
 # Patch YouTube Music Extended 
 get_patches_key "ytmusic"
 get_apkmirror "youtube-music" "arm64-v8a"
 #get_uptodown "youtube-music"
-patch "youtube-music" "yt-music-v$version"
+patch "youtube-music" "yt-music-v$version" "arm64-v8a"
 
 ls revanced-patches*.jar >> revanced-extended-version.txt
 for file in ./*.jar ./*.apk ./*.json
