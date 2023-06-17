2eee31fac2695628ab80ded3fc90c47636aae200
manhduonghn <manhduonghn@live.com>
2023-06-17 19:56:02 +0700

    Add files via upload

--- src/main.sh
+++ src/main.sh
@@ -38,35 +38,36 @@ function dl_gh() {
 
 function get_patches_key() {
     local patch_file="$1"
-    patch_content=($(cat ./patches/$patch_file))
-    exclude_patches=""
-    include_patches=""
+    patch_content=($(cat patches/$patch_file))
     exclude_string=()
     include_string=()
-    flag=0
-    for line in "${patch_content[@]}" ; do
+    exclude_patches=""
+    include_patches=""
+    flag=""
+    for line in "${patch_content[@]}"; do
         if [[ $line == --exclude* ]]; then
-            flag=1
-            continue
-        fi
-        if [[ $line == --include* ]]; then
-            flag=2
-            continue
-        fi
-        if [[ $flag -eq 1 ]]; then
-            exclude_string+=("$line")
-            exclude_patches+="--exclude $line "
-        fi
-        if [[ $flag -eq 2 ]]; then
-            include_string+=("$line")
-            include_patches+="--include $line "
+            flag="exclude"
+            exclude_string+=(${line#--exclude})
+        elif [[ $line == --include* ]]; then
+            flag="include"
+            include_string+=(${line#--include})
+        elif [[ -n $line && $line != --* ]]; then
+            if [[ $flag == "exclude" ]]; then
+                exclude_string+=($line)
+            elif [[ $flag == "include" ]]; then
+                include_string+=($line)
+            fi
         fi
     done
     for patch in "${exclude_string[@]}" ; do
+        exclude_patches+="--exclude $patch "
         if [[ " ${include_string[@]} " =~ " $patch " ]]; then
             printf "\033[0;31mPatch \"%s\" is specified both as exclude and include\033[0m\n" "$patch"
             return 1
         fi
+    done
+    for patch in "${include_string[@]}" ; do
+        include_patches+="--include $patch "
     done
     return 0
 }
