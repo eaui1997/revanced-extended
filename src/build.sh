e480bcadab993ddecb8cd3825c258325d4d3ab62
manhduonghn <manhduonghn@live.com>
2023-06-19 21:48:28 +0700

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -3,24 +3,7 @@
 # Revanced Extended build
 source ./src/main.sh
 
-release=$(curl -sL "https://api.github.com/repos/inotia00/revanced-patches/releases/latest")
-asset=$(echo "$release" | jq -r '.assets[] | select(.name | test("revanced-patches.*\\.jar$")) | .browser_download_url')
-curl -sLO "$asset"
-
-ls revanced-patches*.jar >> new.txt
-rm -f revanced-patches*.jar
-
-release=$(curl -sL "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/latest")
-asset=$(echo "$release" | jq -r '.assets[] | select(.name == "revanced-extended-version.txt") | .browser_download_url')
-curl -sLO "$asset"
-
-if diff -q revanced-extended-version.txt new.txt >/dev/null ; then
-rm -f ./*.txt
-printf "\033[0;31mOld patch!!! Not build\033[0m\n"
-exit 0
-else
-printf "\033[0;32mBuild...\033[0m\n"
-rm -f ./*.txt
+check_new_patch "inotia00" "revanced-extended"
 
 #Download Revanced Extended patches 
 dl_gh "inotia00" "revanced-patches revanced-cli revanced-integrations" "latest"
@@ -43,4 +26,3 @@ ls revanced-patches*.jar >> revanced-extended-version.txt
 for file in ./*.jar ./*.apk ./*.json
    do rm -f "$file"
 done
-fi
