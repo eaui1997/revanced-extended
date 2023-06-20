cf9d7e4694570c128b80174b835e9d322d69d264
manhduonghn <manhduonghn@live.com>
2023-06-20 19:13:47 +0700

    Update main.sh

--- src/main.sh
+++ src/main.sh
@@ -11,7 +11,7 @@ function check_new_patch() {
     release=$(curl -sL "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/latest")
     asset=$(echo "$release" | jq -r '.assets[] | select(.name == "'$txt_name'-version.txt") | .browser_download_url')
     curl -sLO "$asset"
-    if diff -q ${txt_name}-version.txt new.txt >/dev/null
+    if diff -q $txt_name-version.txt new.txt >/dev/null
         then
             rm -f ./*.txt
             printf "\033[0;31mOld patch!!! Not build\033[0m\n"
