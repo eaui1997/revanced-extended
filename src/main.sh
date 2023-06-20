2b3a3a095ca770cfbda3a24bbac49a46f02d1e52
manhduonghn <manhduonghn@live.com>
2023-06-20 17:29:00 +0700

    Update main.sh

--- src/main.sh
+++ src/main.sh
@@ -281,7 +281,7 @@ function split_apk() {
     local apk_name=$1
     local patches_jar=$(find -name "revanced-patches*.jar" -print -quit)
     local cli_jar=$(find -name "revanced-cli*.jar" -print -quit)
-    for arch in "${!arches_to_split[@]}" ; do
+    for arch in "${arches_to_split[@]}" ; do
         printf "\033[0;33mSplitting \033[0;31m\"%s\" \033[0;33m to \033[0;31m\"%s\" \033[0;33m\n" "$apk_name" "$apk_name-$arch"
         java -jar "$cli_jar" \
              --apk "build/$apk_name.apk" \
