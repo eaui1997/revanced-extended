2f7043de88e19158d6bf722dc0a82091cf5fcf92
manhduonghn <manhduonghn@live.com>
2023-06-20 21:56:47 +0700

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -17,11 +17,11 @@ get_apkmirror "youtube"
 patch "youtube" "youtube-revanced-extended"
 
 # Patch YouTube Music Extended 
-#get_patches_key "ytmusic"
-#version="6.04.53"
+get_patches_key "ytmusic"
+version="6.04.53"
 #get_apkmirror "youtube-music" "arm64-v8a"
-#get_uptodown "youtube-music"
-#patch "youtube-music" "youtube-music-revanced-extended"
+get_uptodown "youtube-music"
+patch "youtube-music" "youtube-music-revanced-extended"
 
 # Finish patch
 finish_patch "revanced-extended"
