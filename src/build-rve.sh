ac88b46e8a5055cc49a9110e065d35e015587d8a
manhduonghn <manhduonghn@live.com>
2023-07-31 00:45:24 +0700

    Update build-rve.sh

--- src/build-rve.sh
+++ src/build-rve.sh
@@ -7,25 +7,24 @@ check_new_patch "inotia00" "revanced-extended"
 
 # Download Revanced Extended patches 
 dl_gh "inotia00" "revanced-patches revanced-cli revanced-integrations" "latest"
+
+# Download HTMLQ 
 dl_htmlq
 
 # Patch YouTube Extended
 get_patches_key "youtube"
 get_ver "youtube-rve"
 get_apkmirror "youtube"
-#get_uptodown "youtube"
 patch "youtube" "youtube-revanced-extended-v$version"
 
 # Patch YouTube Music Extended 
 get_patches_key "ytmusic"
 get_apkmirror "youtube-music" "arm64-v8a"
-#get_uptodown "youtube-music"
 patch "youtube-music" "youtube-music-revanced-extended-v$version" "arm64-v8a"
 
 #Reddit
 get_patches_key "reddit"
 get_apkmirror "reddit"
-#get_uptodown "reddit"
 patch "reddit" "reddit-revanced-extended-v$version"
 
 # Finish patch
